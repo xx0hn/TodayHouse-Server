58e8854706ea5edd19748b975b7c40d443a90834
Hwang Seunghwan <64733814+xx0hn@users.noreply.github.com>
2021-08-15 00:46:35 +0900

    Update index.js

--- TodaysHouse-Server/index.js
+++ TodaysHouse-Server/index.js
@@ -1,6 +1,13 @@
 const express = require('./config/express');
 const {logger} = require('./config/winston');
+const app = express();
 
-const port = 3000;
+let port;
+if(process.env.NODE_ENV == 'development'){
+    port = 3000;
+}
+else if(process.env.NODE_ENV == 'production'){
+    port = 3001;
+}
 express().listen(port);
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
